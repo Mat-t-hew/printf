60d91b8ade30e4b770c1db4fb228f21a9bab2bc6
Mat-t-hew <122832898+Mat-t-hew@users.noreply.github.com>
2023-04-04 23:37:19 +0200

    Betty

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
+ * handle_print - prints an argument based on its type
+ * @fmt: formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
@@ -23,26 +23,38 @@ fmt_t fmt_types[] = {
 {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
+
 if (fmt[*ind] == fmt_types[i].fmt)
+
 return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 if (fmt_types[i].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
 return (-1);
 unknow_len += write(1, "%%", 1);
+
 if (fmt[*ind - 1] == ' ')
+
 unknow_len += write(1, " ", 1);
+
 else if (width)
 {
 --(*ind);
+
 while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 --(*ind);
+
 if (fmt[*ind] == ' ')
 --(*ind);
+
 return (1);
+
 }
 unknow_len += write(1, &fmt[*ind], 1);
+
 return (unknow_len);
+
 }
 return (printed_chars);
+
 }
